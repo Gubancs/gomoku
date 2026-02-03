df68a958c87c35e749e49a4f81690da9b27a35a8
Kökény Gábor <kokeny.gabor@smb.hu>
2026-02-03 13:51:11 +0200

    Initial Commit

---
+++ gomokuTests/gomokuTests.swift
@@ -0,0 +1,17 @@
+//
+//  gomokuTests.swift
+//  gomokuTests
+//
+//  Created by Gabor Kokeny on 03/02/2026.
+//
+
+import Testing
+@testable import gomoku
+
+struct gomokuTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
